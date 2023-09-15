690b28a213f9e71059f5b7388028c8921706adb4
Yelena Japarova <yelena.dzhaparova@gmail.com>
2023-09-15 17:03:05 +0200

    modified:   Lessons/03-TYPE/src/interface.ts completed

--- Lessons/03-TYPE/dist/Interface.js
+++ Lessons/03-TYPE/dist/Interface.js
@@ -1,1 +1,12 @@
 "use strict";
+// type Animal = {
+//   name: string
+// }
+const cat = {
+    id: 24,
+    family: "felidae",
+    name: "Stacy",
+    age: 15,
+    breed: "siberian cat"
+};
+console.log(cat);
--- Lessons/03-TYPE/src/interface.ts
+++ Lessons/03-TYPE/src/interface.ts
@@ -1,7 +1,41 @@
-type Animal = {
-  type: "animal"
+// type Animal = {
+//   name: string
+// }
+
+// const cat: Animal = {
+//   name: "Stacy"
+// }
+
+// console.log(cat)
+
+//Перепишем на interface- альтернативное описание обьекта, интерфейс описывает только обьекты
+// Для интерфейсов обязательны фигурные скобки
+
+interface Animal {
   name: string
   age: number
-  habitat: string
-  family: string
 }
+interface Pet {
+  name: string
+  age: number
+}
+
+interface Genius {
+  family: string
+  breed: string
+}
+type Zoo = Animal | Pet
+
+interface AnimalWithGenius extends Animal, Genius {
+  id: number
+}
+
+const cat: AnimalWithGenius = {
+  id: 24,
+  family: "felidae",
+  name: "Stacy",
+  age: 15,
+  breed: "siberian cat"
+}
+
+console.log(cat)
