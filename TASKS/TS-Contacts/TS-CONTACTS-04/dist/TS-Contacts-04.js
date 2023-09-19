bf0567232844d14dc2437308b29474afe67bd6ec
Yelena Japarova <yelena.dzhaparova@gmail.com>
2023-09-19 12:26:32 +0200

    TS-Contacts-04.js completed

---
+++ TASKS/TS-Contacts/TS-CONTACTS-04/dist/TS-Contacts-04.js
@@ -0,0 +1,59 @@
+"use strict";
+const persons = [
+    {
+        name: "Ivan Petrov",
+        age: 27,
+        group: "SEO-specialist"
+    },
+    {
+        name: "Marat Alyautdinov",
+        age: 20,
+        group: "Musician"
+    },
+    {
+        name: "Inna Ivanova",
+        age: 23,
+        group: "Friends"
+    },
+    {
+        name: "Administrator",
+        age: 35,
+        role: "Administrator"
+    },
+    {
+        name: "Anna Sovelyeva",
+        age: 25,
+        group: "Family"
+    },
+    {
+        name: "Petr Davidov",
+        age: 31,
+        group: "Colleague"
+    }
+];
+// Функция для определения типа пользователя
+const getUserType = (user) => {
+    if ("group" in user) {
+        return "User";
+    }
+    else if ("role" in user) {
+        return "Admin";
+    }
+    else {
+        return "Unknown";
+    }
+};
+// Функция для вывода информации о пользователе или администраторе
+const logPerson = (user) => {
+    const userType = getUserType(user);
+    console.log(`Type: ${userType}`);
+    console.log(`${user.name}, ${user.age}`);
+    if ("group" in user) {
+        console.log(`Group: ${user.group}`);
+    }
+    if ("role" in user) {
+        console.log(`Role: ${user.role}`);
+    }
+};
+console.log("Users:");
+persons.forEach(logPerson);
---
+++ TASKS/TS-Contacts/TS-CONTACTS-04/src/TS-Contacts-04.ts
@@ -0,0 +1,76 @@
+interface User {
+  name: string
+  age: number
+  group?: string
+}
+
+interface Admin {
+  name: string
+  age: number
+  role: string
+}
+
+type Person = User | Admin
+
+const persons: Person[] = [
+  {
+    name: "Ivan Petrov",
+    age: 27,
+    group: "SEO-specialist"
+  },
+  {
+    name: "Marat Alyautdinov",
+    age: 20,
+    group: "Musician"
+  },
+  {
+    name: "Inna Ivanova",
+    age: 23,
+    group: "Friends"
+  },
+  {
+    name: "Administrator",
+    age: 35,
+    role: "Administrator"
+  },
+  {
+    name: "Anna Sovelyeva",
+    age: 25,
+    group: "Family"
+  },
+  {
+    name: "Petr Davidov",
+    age: 31,
+    group: "Colleague"
+  }
+]
+
+// Функция для определения типа пользователя
+const getUserType = (user: Person): string => {
+  if ("group" in user) {
+    return "User"
+  } else if ("role" in user) {
+    return "Admin"
+  } else {
+    return "Unknown"
+  }
+}
+
+// Функция для вывода информации о пользователе или администраторе
+const logPerson = (user: Person) => {
+  const userType = getUserType(user)
+
+  console.log(`Type: ${userType}`)
+  console.log(`${user.name}, ${user.age}`)
+
+  if ("group" in user) {
+    console.log(`Group: ${user.group}`)
+  }
+
+  if ("role" in user) {
+    console.log(`Role: ${user.role}`)
+  }
+}
+
+console.log("Users:")
+persons.forEach(logPerson)
