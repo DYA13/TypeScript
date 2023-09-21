2e134078530f4199a4f8fac847bf98f73173ef49
Yelena Japarova <yelena.dzhaparova@gmail.com>
2023-09-21 13:09:52 +0200

    main.ts edited

--- Lessons/04-OOP/dist/main.js
+++ Lessons/04-OOP/dist/main.js
@@ -1,19 +1,80 @@
 "use strict";
+var StatusStudent;
+(function (StatusStudent) {
+    StatusStudent["enrolee"] = "enrolee";
+    StatusStudent["student"] = "student";
+    StatusStudent["graduate"] = "graduate";
+    StatusStudent["bachelor"] = "bachelor";
+})(StatusStudent || (StatusStudent = {}));
 class Student {
+    id = Math.random().toString(32).substring(2, 6) +
+        Date.now().toString().substring(9);
+    status = StatusStudent.enrolee;
+    createAt = new Date();
+    updateAt;
     name;
     course;
-    _module = 1; // восклицательный знак- не обращать внимание на это свойство. Второй способ решения проблемы поставить false  на "strictPropertyInitialization",можно  также поставить восклицательный знак как решение проблемы инициализации , в случае ,если не инициализировано
-    constructor(name, course) {
+    age;
+    _module = 0; // восклицательный знак- не обращать внимание на это свойство. Второй способ решения проблемы поставить false  на "strictPropertyInitialization",можно  также поставить восклицательный знак как решение проблемы инициализации , в случае ,если не инициализировано
+    constructor(name, courseOrAge, age) {
         this.name = name;
-        this.course = course;
-    }
-    get info() {
-        return `${this.name} is studying on the course ${this.course}, on the module ${this._module}`;
+        if (typeof courseOrAge === "string") {
+            this.course = courseOrAge;
+            this.changeStatus(StatusStudent.student);
+        }
+        if (typeof courseOrAge === "number") {
+            this.age = courseOrAge;
+        }
+        if (age) {
+            this.age = age;
+        }
     }
     set module(module) {
         this._module = module;
+        this.updateAt = new Date();
+    }
+    changeStatus(status) {
+        this.status = status;
+        this.updateAt = new Date();
+    }
+    changeInfo(courseOrModule, module) {
+        if (typeof courseOrModule === "string") {
+            this.course = courseOrModule;
+        }
+        if (typeof courseOrModule === "number") {
+            this.module = courseOrModule;
+        }
+        if (module) {
+            this.module = module;
+        }
+        this.updateAt = new Date();
     }
 }
-const student = new Student("Dmitriy", "Frontend");
-console.log("student", student);
-console.log(student.info);
+const student1 = new Student("Petr");
+student1.changeInfo("JS", 4);
+console.log("student1", student1);
+const student2 = new Student("Dmitriy", "Frontend");
+student2.changeInfo("Web");
+console.log("student2", student2);
+const student3 = new Student("Artur", 18);
+student3.changeInfo(2);
+console.log("student3", student3);
+const student4 = new Student("Gennadiy", "JS", 18);
+student4.changeInfo(3);
+console.log("student4", student4);
+//const student: Student = new Student("Dmitriy", "Frontend")
+//console.log("student", student)
+// setTimeout(() => {
+//   student.module = 1
+//   student.status = StatusStudent.student
+//   console.log("student", student)
+// }, 2000)
+// setTimeout(() => {
+//   student.module = 2
+//   console.log("student", student)
+// }, 5000)
+// setTimeout(() => {
+//   student.module = 3
+//   student.status = StatusStudent.graduate
+//   console.log("student", student)
+// }, 7500)
--- Lessons/04-OOP/src/main.ts
+++ Lessons/04-OOP/src/main.ts
@@ -1,22 +1,96 @@
+enum StatusStudent {
+  enrolee = "enrolee",
+  student = "student",
+  graduate = "graduate",
+  bachelor = "bachelor"
+}
 class Student {
+  id: string =
+    Math.random().toString(32).substring(2, 6) +
+    Date.now().toString().substring(9)
+  status: StatusStudent = StatusStudent.enrolee
+  createAt: Date = new Date()
+  updateAt?: Date
   name: string
-  course: string
-  _module: number = 1 // восклицательный знак- не обращать внимание на это свойство. Второй способ решения проблемы поставить false  на "strictPropertyInitialization",можно  также поставить восклицательный знак как решение проблемы инициализации , в случае ,если не инициализировано
+  course?: string
+  age?: number
+  _module: number = 0 // восклицательный знак- не обращать внимание на это свойство. Второй способ решения проблемы поставить false  на "strictPropertyInitialization",можно  также поставить восклицательный знак как решение проблемы инициализации , в случае ,если не инициализировано
 
-  constructor(name: string, course: string) {
+  // перегрузка
+  constructor(name: string)
+  constructor(name: string, course: string)
+  constructor(name: string, age: number)
+  constructor(name: string, course: string, age: number)
+
+  constructor(name: string, courseOrAge?: string | number, age?: number) {
     this.name = name
-    this.course = course
+    if (typeof courseOrAge === "string") {
+      this.course = courseOrAge
+      this.changeStatus(StatusStudent.student)
+    }
+    if (typeof courseOrAge === "number") {
+      this.age = courseOrAge
+    }
+    if (age) {
+      this.age = age
+    }
   }
-  get info(): string {
-    return `${this.name} is studying on the course ${this.course}, on the module ${this._module}`
-  }
+
   set module(module: number) {
     this._module = module
+    this.updateAt = new Date()
+  }
+  changeStatus(status: StatusStudent): void {
+    this.status = status
+    this.updateAt = new Date()
+  }
+  changeInfo(course: string): void
+  changeInfo(module: number): void
+  changeInfo(course: string, module: number): void
+  changeInfo(courseOrModule: string | number, module?: number): void {
+    if (typeof courseOrModule === "string") {
+      this.course = courseOrModule
+    }
+    if (typeof courseOrModule === "number") {
+      this.module = courseOrModule
+    }
+    if (module) {
+      this.module = module
+    }
+    this.updateAt = new Date()
   }
 }
+const student1: Student = new Student("Petr")
+student1.changeInfo("JS", 4)
+console.log("student1", student1)
 
-const student: Student = new Student("Dmitriy", "Frontend")
+const student2: Student = new Student("Dmitriy", "Frontend")
+student2.changeInfo("Web")
+console.log("student2", student2)
 
-console.log("student", student)
+const student3: Student = new Student("Artur", 18)
+student3.changeInfo(2)
+console.log("student3", student3)
 
-console.log(student.info)
+const student4: Student = new Student("Gennadiy", "JS", 18)
+student4.changeInfo(3)
+console.log("student4", student4)
+
+//const student: Student = new Student("Dmitriy", "Frontend")
+//console.log("student", student)
+// setTimeout(() => {
+//   student.module = 1
+//   student.status = StatusStudent.student
+//   console.log("student", student)
+// }, 2000)
+
+// setTimeout(() => {
+//   student.module = 2
+//   console.log("student", student)
+// }, 5000)
+
+// setTimeout(() => {
+//   student.module = 3
+//   student.status = StatusStudent.graduate
+//   console.log("student", student)
+// }, 7500)
