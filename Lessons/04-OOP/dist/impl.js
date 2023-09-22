8af17e49776cf2b61caa6cb55728606ec38f9eb1
Yelena Japarova <yelena.dzhaparova@gmail.com>
2023-09-22 15:09:19 +0200

    impl.ts edited

--- Lessons/04-OOP/dist/impl.js
+++ Lessons/04-OOP/dist/impl.js
@@ -1,1 +1,31 @@
 "use strict";
+class TV {
+    title;
+    id = +Date.now.toString().substring(9);
+    count = 0;
+    price;
+    constructor(title, price, count = 0) {
+        this.title = title;
+        this.count = count;
+        this.price = price;
+    }
+    logger(str) {
+        console.log(str + this.title);
+    }
+}
+class WM {
+    title;
+    id = +Date.now.toString().substring(9);
+    count = 0;
+    price;
+    weight;
+    constructor(title, price, count = 0, weight) {
+        this.title = title;
+        this.count = count;
+        this.price = price;
+        this.weight = weight;
+    }
+    logger(str) {
+        console.log(str + this.title);
+    }
+}
