9f72953ae4c1e284c3ca761fea5f1cd8460eafed
Yelena Japarova <yelena.dzhaparova@gmail.com>
2023-10-03 13:10:12 +0200

    TS-GENERICS-02/src/main.ts done

---
+++ TASKS/TS-Contacts/TS-GENERICS-02/dist/main.js
@@ -0,0 +1,71 @@
+"use strict";
+//Создать абстрактный класс Users, который сожержит список пользователей userList
+class Users {
+    constructor() {
+        this.userList = [];
+    }
+    //add добавляет пользователя в список userList
+    add(user) {
+        this.userList.push(user);
+    }
+    //remove удаляет по id из списка userList и возвращает булево значение true если пользователь был удален и false если пользователь не был найден
+    remove(id) {
+        const index = this.userList.findIndex((user) => user.id === id);
+        if (index !== -1) {
+            this.userList.splice(index, 1);
+            return true;
+        }
+        return false;
+    }
+    //get возвращает пользователя по id или null если пользователь не был найден
+    get(id) {
+        const user = this.userList.find((u) => u.id === id);
+        return user || null;
+    }
+    // метод sorted который возвращает отсортированный список userList
+    //Метод принимает один параметр, тип сортировки, по возрастанию или по убыванию, по умолчанию значение по возрастанию
+    sorted(order) {
+        const sortedList = [...this.userList];
+        if (order === "по возрастанию") {
+            sortedList.sort((a, b) => a.id - b.id);
+        }
+        else {
+            sortedList.sort((a, b) => b.id - a.id);
+        }
+        return sortedList;
+    }
+}
+//создать два класса наследуя Users , Students и Employees
+//у студентов обязательные поля  year: номер курса , specialty: специальность
+class Students extends Users {
+    constructor(id, firstname, surname, age, year, speciality) {
+        super();
+        this.year = year;
+        this.speciality = speciality;
+        this.add({ id, firstname, surname, age });
+    }
+}
+// у сотрудников post: должность
+class Employees extends Users {
+    constructor(id, firstname, surname, age, post) {
+        super();
+        this.post = post;
+        this.add({ id, firstname, surname, age });
+    }
+}
+//добавьте 2 сотрудника и 5 студентов
+const students = [
+    new Students(1, "Ivan", "Ivanov", 20, 2, "Computer Science"),
+    new Students(1, "Petr", "Petrov", 24, 5, "Math"),
+    new Students(1, "Maria", "Sidorova", 33, 1, "History"),
+    new Students(1, "Tatyana", "Smirnova", 56, 7, "Chemistry"),
+    new Students(1, "Alexey", "Medvedev", 30, 9, "Phisics")
+];
+const employees = [
+    new Employees(101, "Anna", "Zaitseva", 45, "Teacher"),
+    new Employees(50, "Dmitriy", "Kozlov", 50, "Head of the department")
+];
+const sortedStudents = students[0].sorted("по возрастанию");
+console.log("Sorted Students (По возрастанию):", sortedStudents);
+const sortedEmployees = employees[0].sorted("по убыванию");
+console.log("Sorted Employees (По убыванию):", sortedEmployees);
