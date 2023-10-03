a0f2b66d25dd16711e8ee21dd9979da3665c09e3
Yelena Japarova <yelena.dzhaparova@gmail.com>
2023-10-03 17:20:40 +0200

    TS-Contacts-04.ts modified

--- TASKS/TS-Contacts/TS-CONTACTS-04/src/TS-Contacts-04.ts
+++ TASKS/TS-Contacts/TS-CONTACTS-04/src/TS-Contacts-04.ts
@@ -1,86 +1,72 @@
-interface User {
-  name: string
-  age: number
-  group?: string
-}
-
-interface Admin {
-  name: string
-  age: number
-  role: string
-}
-
-type Person = User | Admin
-
-const persons: Person[] = [
-  {
-    name: "Ivan Petrov",
-    age: 27,
-    group: "SEO-specialist"
-  },
-  {
-    name: "Marat Alyautdinov",
-    age: 20,
-    group: "Musician"
-  },
-  {
-    name: "Inna Ivanova",
-    age: 23,
-    group: "Friends"
-  },
-  {
-    name: "Administrator",
-    age: 35,
-    role: "Administrator"
-  },
-  {
-    name: "Anna Sovelyeva",
-    age: 25,
-    group: "Family"
-  },
-  {
-    name: "Petr Davidov",
-    age: 31,
-    group: "Colleague"
-  }
-]
-
-// Функция для определения, является ли пользователь администратором
-const isAdmin = (user: Person): user is Admin => {
-  return (user as Admin).role !== undefined
-}
-
-// Функция для определения, является ли пользователь обычным пользователем
-const isUser = (user: Person): user is User => {
-  return (user as User).group !== undefined
-}
-
-// Функция для определения типа пользователя
-const getUserType = (user: Person): string => {
-  if ("group" in user) {
-    return "User"
-  } else if ("role" in user) {
-    return "Admin"
-  } else {
-    return "Unknown"
-  }
-}
-
-// Функция для вывода информации о пользователе или администраторе
-const logPerson = (user: Person) => {
-  const userType = getUserType(user)
-
-  console.log(`Type: ${userType}`)
-  console.log(`${user.name}, ${user.age}`)
-
-  if ("group" in user) {
-    console.log(`Group: ${user.group}`)
+function main() {
+  interface User {
+    name: string
+    age: number
+    group?: string
   }
 
-  if ("role" in user) {
-    console.log(`Role: ${user.role}`)
+  interface Admin {
+    name: string
+    age: number
+    role: string
   }
+
+  type Person = User | Admin
+
+  const persons: Person[] = [
+    {
+      name: "Ivan Petrov",
+      age: 27,
+      group: "SEO-specialist"
+    },
+    {
+      name: "Marat Alyautdinov",
+      age: 20,
+      group: "Musician"
+    },
+    {
+      name: "Inna Ivanova",
+      age: 23,
+      group: "Friends"
+    },
+    {
+      name: "Administrator",
+      age: 35,
+      role: "Administrator"
+    },
+    {
+      name: "Anna Sovelyeva",
+      age: 25,
+      group: "Family"
+    },
+    {
+      name: "Petr Davidov",
+      age: 31,
+      group: "Colleague"
+    }
+  ]
+
+  const logPerson = (user: Person) => {
+    console.log(`${user.name}, ${user.age}`)
+
+    if ("group" in user) {
+      console.log(`Group: ${user.group}`)
+    }
+
+    if ("role" in user) {
+      console.log(`Role: ${user.role}`)
+    }
+  }
+
+  console.log("Users:")
+  const users: User[] = persons.filter((person) => "group" in person) as User[]
+  users.forEach(logPerson)
+
+  console.log("Admins:")
+  const admins: Admin[] = persons.filter(
+    (person) => "role" in person
+  ) as Admin[]
+  admins.forEach(logPerson)
 }
 
-console.log("Users:")
-persons.forEach(logPerson)
+main()
